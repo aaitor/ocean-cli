0780037f9cbe17d1619a877bf6f9c9418a0ecfed
Aitor <aaitor@gmail.com>
2019-04-30 13:10:07 +0200

    making token handlers callable

--- src/main/java/com/bigchaindb/ocean/cli/modules/tokens/TokensRequest.java
+++ src/main/java/com/bigchaindb/ocean/cli/modules/tokens/TokensRequest.java
@@ -1,19 +1,22 @@
 package com.bigchaindb.ocean.cli.modules.tokens;
 
 import com.bigchaindb.ocean.cli.TokensCLI;
+import com.bigchaindb.ocean.cli.models.CommandResult;
+import com.bigchaindb.ocean.cli.models.exceptions.CLIException;
 import com.oceanprotocol.squid.exceptions.EthereumException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import picocli.CommandLine;
 
 import java.math.BigInteger;
+import java.util.concurrent.Callable;
 
 import static com.bigchaindb.ocean.cli.helpers.Constants.TransactionSuccess;
 
 @CommandLine.Command(
         name = "request",
         description = "Request some TokensCLI")
-public class TokensRequest implements Runnable {
+public class TokensRequest implements Callable {
 
     private static final Logger log = LogManager.getLogger(TokensRequest.class);
 
@@ -26,25 +29,33 @@ public class TokensRequest implements Runnable {
     @CommandLine.Parameters(index = "0")
     BigInteger numberTokens;
 
-    void request() {
+    CommandResult request() throws CLIException {
         try {
-            log.info("Requesting " + numberTokens.longValue() +
-                    " tokens for " + parent.cli.getOceanAPI().getMainAccount().getAddress() +
+            System.out.println("Requesting " + numberTokens.longValue() +
+                    " Ocean Token/s for " + parent.cli.getOceanAPI().getMainAccount().getAddress() +
                     " address");
+
+            parent.cli.progressBar.start();
 
             String status= parent.cli.getOceanAPI().getTokensAPI()
                     .request(numberTokens)
                     .getStatus();
 
             if (status.equals(TransactionSuccess))
-                System.out.println("Success!");
+                System.out.println("\nSuccess!\n");
+
         } catch (EthereumException e) {
             log.error(e.getMessage());
+            throw new CLIException(e.getMessage());
+        } finally {
+            parent.cli.progressBar.doStop();
         }
+        return CommandResult.successResult();
+
     }
 
     @Override
-    public void run() {
-        request();
+    public CommandResult call() throws CLIException {
+        return request();
     }
 }
